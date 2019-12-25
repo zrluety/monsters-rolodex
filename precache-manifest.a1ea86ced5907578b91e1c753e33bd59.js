a8d863e438083c7fa8b419076d0f55531e3dd841
Zach Luety <zluety@gpwa.com>
2019-12-25 11:48:05 -0700

    Updates

---
+++ precache-manifest.a1ea86ced5907578b91e1c753e33bd59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "349c1c4616275d476ea6f596c17e36c9",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "e2b5d01642c846bc35ca",
+    "url": "/monsters-rolodex/static/css/main.dc6707f2.chunk.css"
+  },
+  {
+    "revision": "4ee796cfabedc5bd953d",
+    "url": "/monsters-rolodex/static/js/2.cd5ddd3b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.cd5ddd3b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e2b5d01642c846bc35ca",
+    "url": "/monsters-rolodex/static/js/main.e88f76f6.chunk.js"
+  },
+  {
+    "revision": "112da8816d1bd2c60f7c",
+    "url": "/monsters-rolodex/static/js/runtime-main.3a6486ae.js"
+  }
+]);
